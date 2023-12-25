8c42cb358142a76097b60959cb3ac3482f8ea866
antoniomrrds <arqueiroverd@gmail.com>
2023-12-25 13:51:01 -0300

    feat: ✨ensure FacebookAuthenticationService update account when LoadUserAccountRepository returns data

--- src/application/ports/repositories/index.ts
+++ src/application/ports/repositories/index.ts
@@ -1,2 +1,3 @@
 export * from './load-user-account'
 export * from './create-facebook-account'
+export * from './update-facebook-account-repository'
--- src/application/ports/repositories/load-user-account.ts
+++ src/application/ports/repositories/load-user-account.ts
@@ -1,8 +1,12 @@
 export interface LoadUserAccountRepository {
-  load: (params: LoadUserAccountRepository.Params) => Promise<void>
+  load: (params: LoadUserAccountRepository.Params) => Promise<LoadUserAccountRepository.Result>
 }
 namespace LoadUserAccountRepository {
   export type Params = {
     email: string
+  }
+  export type Result = undefined | {
+    id: string
+    name?: string
   }
 }
