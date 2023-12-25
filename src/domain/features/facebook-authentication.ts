9a99f32a083f23be7d755e21b0659cef6cb333fc
antoniomrrds <arqueiroverd@gmail.com>
2023-12-25 02:48:54 -0300

    feat: ✨add facebook authentication feature

---
+++ src/domain/features/facebook-authentication.ts
@@ -0,0 +1,16 @@
+import { AccessToken } from '@/domain/entities'
+import { AuthenticationError } from '@/domain/errors'
+
+export interface FacebookAuthentication {
+  perform: (
+    params: FacebookAuthentication.Params,
+  ) => Promise<FacebookAuthentication.Result>
+}
+
+export namespace FacebookAuthentication {
+  export type Params = {
+    token: string
+  }
+
+  export type Result = AccessToken | AuthenticationError
+}
