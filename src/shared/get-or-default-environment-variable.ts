9c1fc628f27b5ce8860eeaedfea31bd165a0ff68
antoniomrrds <arqueiroverd@gmail.com>
2023-12-25 02:26:43 -0300

    feat: ✨ensure returns the default value if the environment variable is empty

--- src/shared/get-or-default-environment-variable.ts
+++ src/shared/get-or-default-environment-variable.ts
@@ -1,12 +1,15 @@
 import { EnvironmentVariableError } from '@/shared/errors'
 
-export type EnvironmentVariableGetter = string | Error
+export type EnvironmentVariableGetter = string | Error | undefined
 
 export const getOrDefaultEnvironmentVariable = (
   environmentVariable: string,
   defaultValue?: string
 ): EnvironmentVariableGetter => {
-  const value = process.env[environmentVariable] ?? defaultValue
-  if (!value) return new EnvironmentVariableError(environmentVariable)
+  const value = process.env[environmentVariable]
+  if (!value && defaultValue) return defaultValue
+  if (!value && !defaultValue) {
+    return new EnvironmentVariableError(environmentVariable)
+  }
   return value
 }
