1e9d2f349757435c948df6b830617953733b9054
antoniomrrds <arqueiroverd@gmail.com>
2023-12-25 02:17:44 -0300

    feat: ✨ensure returns the default value if the environment variable is not defined

--- src/shared/get-or-default-environment-variable.ts
+++ src/shared/get-or-default-environment-variable.ts
@@ -1,8 +1,9 @@
 export type EnvironmentVariableGetter = string
 
 export const getOrDefaultEnvironmentVariable = (
-  environmentVariable: string
+  environmentVariable: string,
+  defaultValue?: string
 ): EnvironmentVariableGetter => {
-  const value = process.env[environmentVariable]
+  const value = process.env[environmentVariable] ?? defaultValue
   return value!
 }
