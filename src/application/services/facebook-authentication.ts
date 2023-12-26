f08ae6487b2f1f9825b66abf15d344f83a2e6bab
antoniomrrds <arqueiroverd@gmail.com>
2023-12-25 22:10:04 -0300

    refactor: 📦move business login to a domain entity

--- src/application/services/facebook-authentication.ts
+++ src/application/services/facebook-authentication.ts
@@ -3,6 +3,7 @@ import {
   LoadUserAccountRepository,
   SaveFacebookAccountRepositry
 } from '@/application/ports'
+import { FacebookAccount } from '@/domain/entities'
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 
@@ -16,12 +17,8 @@ export class FacebookAuthenticationService {
     const fbData = await this.facebookApi.loadUser(params)
     if (fbData !== undefined) {
       const accountData = await this.userAccountRepo.load({ email: fbData.email })
-      await this.userAccountRepo.saveWithFacebook({
-        id: accountData?.id,
-        name: accountData?.name ?? fbData.name,
-        email: fbData.email,
-        facebookId: fbData.facebookId
-      })
+      const fbAccount = new FacebookAccount(fbData, accountData)
+      await this.userAccountRepo.saveWithFacebook(fbAccount)
     }
     return new AuthenticationError()
   }
---
+++ tests/domain/entities/facebook-account.spec.ts
@@ -0,0 +1,43 @@
+import { FacebookAccount } from '@/domain/entities'
+
+describe('FacebookAccount', () => {
+  const fbData = {
+    name: 'any_fb_name',
+    email: 'any_fb_email',
+    facebookId: 'any_fb_id'
+  }
+  it('Should create with facebook data only', () => {
+    const sut = new FacebookAccount(fbData)
+
+    expect(sut).toEqual({
+      name: 'any_fb_name',
+      email: 'any_fb_email',
+      facebookId: 'any_fb_id'
+    })
+  })
+  it('Should update name if its empty', () => {
+    // arrange
+    const accountData = { id: 'any_id' }
+    // act
+    const sut = new FacebookAccount(fbData, accountData)
+    // assert
+    expect(sut).toEqual({
+      id: 'any_id',
+      name: 'any_fb_name',
+      email: 'any_fb_email',
+      facebookId: 'any_fb_id'
+    })
+  })
+  it('Should not update name if its not empty', () => {
+    const accountData = { id: 'any_id', name: 'any_name' }
+
+    const sut = new FacebookAccount(fbData, accountData)
+
+    expect(sut).toEqual({
+      id: 'any_id',
+      name: 'any_name',
+      email: 'any_fb_email',
+      facebookId: 'any_fb_id'
+    })
+  })
+})
