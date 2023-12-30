0d86cb677c49e440244b918e82f991ab61ec2e8f
antoniomrrds <arqueiroverd@gmail.com>
2023-12-30 12:53:32 -0300

    feat: ​​🧩 ensure  JwtTokenGenerator  returns token  on success

--- src/infrastructure/crypto/jwt-token-generator.ts
+++ src/infrastructure/crypto/jwt-token-generator.ts
@@ -4,7 +4,6 @@ export class JwtTokenGenerator implements TokenGenerator {
   constructor (private readonly secret: string) {}
   async generateToken (params: TokenGenerator.Params): Promise<TokenGenerator.Result> {
     const expirationInSeconds = params.expirationInMs / 1000
-    sign({ key: params.key }, this.secret, { expiresIn: expirationInSeconds })
-    return 'any_value'
+    return sign({ key: params.key }, this.secret, { expiresIn: expirationInSeconds })
   }
 }
