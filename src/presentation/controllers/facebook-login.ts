140a230e2e58259747948fe0545f9370c015975b
antoniomrrds <arqueiroverd@gmail.com>
2024-01-01 23:50:56 -0300

    feat: ​​🧩 ensure FacebookLoginController returns 500 if authentication throws

--- src/presentation/controllers/facebook-login.ts
+++ src/presentation/controllers/facebook-login.ts
@@ -1,29 +1,37 @@
 import { AccessToken } from '@/domain/entities'
 import { FacebookAuthentication } from '@/domain/features'
+import { ServerError } from '@/presentation/errors'
 import { HttpResponse } from '@/presentation/ports'
 
 export class FacebookLoginController {
   constructor (private readonly facebookAuth: FacebookAuthentication) {}
   async handle (httpRequest: any): Promise<HttpResponse> {
+    try {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    if (['', null, undefined].includes(httpRequest.token)) {
-      return {
-        statusCode: 400,
-        data: new Error('The field token is required')
-      }
-    }
-    const result = await this.facebookAuth.perform({ token: httpRequest.token })
-    if (result instanceof AccessToken) {
-      return {
-        statusCode: 200,
-        data: {
-          accessToken: result.value
+      if (['', null, undefined].includes(httpRequest.token)) {
+        return {
+          statusCode: 400,
+          data: new Error('The field token is required')
         }
       }
-    } else {
+      const result = await this.facebookAuth.perform({ token: httpRequest.token })
+      if (result instanceof AccessToken) {
+        return {
+          statusCode: 200,
+          data: {
+            accessToken: result.value
+          }
+        }
+      } else {
+        return {
+          statusCode: 401,
+          data: result
+        }
+      }
+    } catch (error) {
       return {
-        statusCode: 401,
-        data: result
+        statusCode: 500,
+        data: new ServerError(error instanceof Error ? error : undefined)
       }
     }
   }
