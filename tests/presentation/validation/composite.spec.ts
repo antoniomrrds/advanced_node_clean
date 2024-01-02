baa2bd0a8464ad25403f12d96028864319730e4a
antoniomrrds <arqueiroverd@gmail.com>
2024-01-02 17:55:47 -0300

    refactor: 📦 isolate SUT creation

--- tests/presentation/validation/composite.spec.ts
+++ tests/presentation/validation/composite.spec.ts
@@ -1,15 +1,23 @@
 import { Validator, ValidationComposite } from '@/presentation/validation'
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('ValidationComposite', () => {
-  it('Should return undefined if all validators return undefined', () => {
-    const validator1 = mock<Validator>()
-    validator1.validate.mockReturnValue(undefined)
-    const validator2 = mock<Validator>()
-    validator2.validate.mockReturnValue(undefined)
-    const validators = [validator1, validator2]
-    const sut = new ValidationComposite(validators)
+  let sut: ValidationComposite
+  let validator1: MockProxy<Validator>
+  let validator2: MockProxy<Validator>
+  let validators: Validator[]
 
+  beforeEach(() => {
+    sut = new ValidationComposite(validators)
+  })
+  beforeAll(() => {
+    validator1 = mock()
+    validator1.validate.mockReturnValue(undefined)
+    validator2 = mock()
+    validator2.validate.mockReturnValue(undefined)
+    validators = [validator1, validator2]
+  })
+  it('Should return undefined if all validators return undefined', () => {
     // eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
     const error = sut.validate()
 
