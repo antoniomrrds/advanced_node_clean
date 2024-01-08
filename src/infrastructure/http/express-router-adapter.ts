2115747f4c382494e210bc9c0bc0a42e743cde16
antoniomrrds <arqueiroverd@gmail.com>
2024-01-08 00:49:00 -0300

    feat: ​​🧩ensure ExpressRouterAdapter responds with 200 and valid data

--- src/infrastructure/http/express-router-adapter.ts
+++ src/infrastructure/http/express-router-adapter.ts
@@ -3,6 +3,7 @@ import { Controller } from '@/presentation/controllers'
 export class ExpressRouterAdapter {
   constructor (private readonly controller: Controller) {}
   async adapt (req: Request, res: Response): Promise<void> {
-    await this.controller.handle({ ...req.body })
+    const httpResponse = await this.controller.handle({ ...req.body })
+    res.status(200).json(httpResponse.body)
   }
 }
