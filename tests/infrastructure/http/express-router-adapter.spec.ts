1cb02a44b7706ffb2f665290dbf17ceed988d9a9
antoniomrrds <arqueiroverd@gmail.com>
2024-01-08 00:38:44 -0300

    refactor: 📦 isolate SUT Creation

--- tests/infrastructure/http/express-router-adapter.spec.ts
+++ tests/infrastructure/http/express-router-adapter.spec.ts
@@ -1,14 +1,22 @@
 import { getMockReq, getMockRes } from '@jest-mock/express'
 import { Controller } from '@/presentation/controllers'
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 import { ExpressRouterAdapter } from '@/infrastructure/http'
+import { Request, Response } from 'express'
 
 describe('ExpressRouterAdapter', () => {
+  let req: Request
+  let res: Response
+  let controller: MockProxy<Controller>
+  let sut: ExpressRouterAdapter
+
+  beforeEach(() => {
+    req = getMockReq({ body: { any: 'any' } })
+    res = getMockRes().res
+    controller = mock()
+    sut = new ExpressRouterAdapter(controller)
+  })
   it('Should call handle with correct request', async () => {
-    const req = getMockReq({ body: { any: 'any' } })
-    const { res } = getMockRes()
-    const controller = mock<Controller>()
-    const sut = new ExpressRouterAdapter(controller)
     await sut.adapt(req, res)
 
     // eslint-disable-next-line @typescript-eslint/unbound-method
@@ -16,9 +24,7 @@ describe('ExpressRouterAdapter', () => {
   })
   it('Should call handle with empty request', async () => {
     const req = getMockReq()
-    const { res } = getMockRes()
-    const controller = mock<Controller>()
-    const sut = new ExpressRouterAdapter(controller)
+
     await sut.adapt(req, res)
 
     // eslint-disable-next-line @typescript-eslint/unbound-method
