72a750be7a5b19395475c59ae46e93f433e3c796
antoniomrrds <arqueiroverd@gmail.com>
2024-01-12 00:21:08 -0300

    feat: ​​🧩 ensure Authorize returns accessToken

--- src/domain/ports/crypto/token-validator.ts
+++ src/domain/ports/crypto/token-validator.ts
@@ -1,7 +1,8 @@
 export interface TokenValidator {
-  validateToken: (params: TokenValidator.Params) => Promise<void>
+  validateToken: (params: TokenValidator.Params) => Promise<TokenValidator.Result>
 }
 
 export namespace TokenValidator {
   export type Params = { token: string }
+  export type Result = string
 }
--- src/domain/use-cases/authorize.ts
+++ src/domain/use-cases/authorize.ts
@@ -2,8 +2,10 @@ import { TokenValidator } from '@/domain/ports'
 
 type Setup = (crypto: TokenValidator) => Authorize
 type Input = { token: string }
-export type Authorize = (params: Input) => Promise<void>
+type Output = string
+
+export type Authorize = (params: Input) => Promise<Output>
 
 export const setupAuthorize: Setup = crypto => async params => {
-  await crypto.validateToken(params)
+  return crypto.validateToken(params)
 }
