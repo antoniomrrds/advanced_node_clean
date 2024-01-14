045653a1bd911a64c01767095629fca2b59fa7c4
antoniomrrds <arqueiroverd@gmail.com>
2024-01-14 01:34:13 -0300

    feat: ​​🧩  ensure AuthenticationMiddleware calls  authorize with correct params

--- src/presentation/middlewares/authentication.ts
+++ src/presentation/middlewares/authentication.ts
@@ -1,10 +1,16 @@
+import { Authorize } from '@/domain/use-cases'
 import { forbidden } from '@/presentation/helpers'
 import { HttpResponse } from '@/presentation/ports'
+import { RequiredStringValidator } from '@/presentation/validation'
 
 type HttpRequest = { authorization: string }
 
 export class AuthenticationMiddleware {
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse<Error>> {
-    return forbidden()
+  constructor (private readonly authorize: Authorize) {}
+
+  async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Error> | undefined> {
+    const errror = new RequiredStringValidator(authorization, 'authorization').validate()
+    if (errror !== undefined) return forbidden()
+    await this.authorize({ token: authorization })
   }
 }
