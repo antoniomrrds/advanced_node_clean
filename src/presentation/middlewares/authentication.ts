2a0464886750597317404a7ed417833acb89d745
antoniomrrds <arqueiroverd@gmail.com>
2024-01-14 01:50:59 -0300

    refactor: 📦improve code readability

--- src/presentation/middlewares/authentication.ts
+++ src/presentation/middlewares/authentication.ts
@@ -11,12 +11,16 @@ export class AuthenticationMiddleware {
 
   async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Model>> {
     try {
-      const errror = new RequiredStringValidator(authorization, 'authorization').validate()
-      if (errror !== undefined) return forbidden()
+      if (!this.validate({ authorization })) return forbidden()
       const userId = await this.authorize({ token: authorization })
       return ok({ userId })
     } catch {
       return forbidden()
     }
+  }
+
+  private validate ({ authorization }: HttpRequest): boolean {
+    const errror = new RequiredStringValidator(authorization, 'authorization').validate()
+    return errror === undefined
   }
 }
