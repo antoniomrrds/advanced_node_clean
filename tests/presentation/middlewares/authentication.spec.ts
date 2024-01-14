14aee4655777edff9708e089fbf470f8318aa6b5
antoniomrrds <arqueiroverd@gmail.com>
2024-01-14 01:34:40 -0300

    test: 🧪ensure AuthenticationMiddleware calls  authorize with correct params

--- tests/presentation/middlewares/authentication.spec.ts
+++ tests/presentation/middlewares/authentication.spec.ts
@@ -3,12 +3,21 @@ import { AuthenticationMiddleware } from '@/presentation/middlewares'
 
 describe('AuthenticationMiddleware', () => {
   let sut: AuthenticationMiddleware
+  let authorization: string
+  let authorize: jest.Mock
 
   beforeEach(() => {
-    sut = new AuthenticationMiddleware()
+    sut = new AuthenticationMiddleware(authorize)
   })
+
+  beforeAll(() => {
+    authorization = 'any_authorization_token'
+    authorize = jest.fn()
+  })
+
   it('Should return 403 if authorization is empty', async () => {
     const httpResponse = await sut.handle({ authorization: '' })
+
     expect(httpResponse).toEqual({
       statusCode: 403,
       body: new ForbiddenError()
@@ -16,6 +25,7 @@ describe('AuthenticationMiddleware', () => {
   })
   it('Should return 403 if authorization is null', async () => {
     const httpResponse = await sut.handle({ authorization: null as any })
+
     expect(httpResponse).toEqual({
       statusCode: 403,
       body: new ForbiddenError()
@@ -23,9 +33,16 @@ describe('AuthenticationMiddleware', () => {
   })
   it('Should return 403 if authorization is undefined', async () => {
     const httpResponse = await sut.handle({ authorization: undefined as any })
+
     expect(httpResponse).toEqual({
       statusCode: 403,
       body: new ForbiddenError()
     })
+  })
+  it('Should call authorization with correct params', async () => {
+    await sut.handle({ authorization })
+
+    expect(authorize).toHaveBeenCalledWith({ token: authorization })
+    expect(authorize).toHaveBeenCalledTimes(1)
   })
 })
