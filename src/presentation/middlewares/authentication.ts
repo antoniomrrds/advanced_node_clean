e6c46d2447c1ed4d1c29c50691e468e013a01576
antoniomrrds <arqueiroverd@gmail.com>
2024-01-14 01:45:42 -0300

    feat: ​​🧩ensure AuthenticationMiddleware returns 200 with userId

--- src/presentation/middlewares/authentication.ts
+++ src/presentation/middlewares/authentication.ts
@@ -1,18 +1,20 @@
 import { Authorize } from '@/domain/use-cases'
-import { forbidden } from '@/presentation/helpers'
+import { forbidden, ok } from '@/presentation/helpers'
 import { HttpResponse } from '@/presentation/ports'
 import { RequiredStringValidator } from '@/presentation/validation'
 
 type HttpRequest = { authorization: string }
+type Model = Error | { userId: string }
 
 export class AuthenticationMiddleware {
   constructor (private readonly authorize: Authorize) {}
 
-  async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Error> | undefined> {
+  async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Model>> {
     try {
       const errror = new RequiredStringValidator(authorization, 'authorization').validate()
       if (errror !== undefined) return forbidden()
-      await this.authorize({ token: authorization })
+      const userId = await this.authorize({ token: authorization })
+      return ok({ userId })
     } catch {
       return forbidden()
     }
