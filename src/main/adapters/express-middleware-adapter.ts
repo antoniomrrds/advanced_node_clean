5cf3cbee0bc1ef79bb018fe227e2ebfcbf71aebc
antoniomrrds <arqueiroverd@gmail.com>
2024-01-14 23:18:25 -0300

    feat: ​​🧩ensure ExpressMiddleware add data to req.locals

--- src/main/adapters/express-middleware-adapter.ts
+++ src/main/adapters/express-middleware-adapter.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { Middleware } from '@/presentation/middlewares'
 import { RequestHandler } from 'express'
 
@@ -5,5 +6,12 @@ type Adapter = (middleware: Middleware) => RequestHandler
 
 export const adaptExpressMiddleware: Adapter = middleware => async (req, res, next) => {
   const { statusCode, body } = await middleware.handle({ ...req.headers })
-  return res.status(statusCode).json(body)
+  if (statusCode === 200) {
+    const entries = Object.entries(body).filter(entry => entry[1])
+
+    req.locals = { ...req.locals, ...Object.fromEntries(entries) }
+    next()
+  } else {
+    res.status(statusCode).json(body)
+  }
 }
