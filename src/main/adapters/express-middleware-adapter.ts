505c680025cfcc0512700e699b7a9fd4fe1e21c3
antoniomrrds <arqueiroverd@gmail.com>
2024-01-14 23:02:34 -0300

    feat: ​​🧩 ensure ExpressMiddleware respond with correct  error and statusCode

--- src/main/adapters/express-middleware-adapter.ts
+++ src/main/adapters/express-middleware-adapter.ts
@@ -4,5 +4,6 @@ import { RequestHandler } from 'express'
 type Adapter = (middleware: Middleware) => RequestHandler
 
 export const adaptExpressMiddleware: Adapter = middleware => async (req, res, next) => {
-  await middleware.handle({ ...req.headers })
+  const { statusCode, body } = await middleware.handle({ ...req.headers })
+  return res.status(statusCode).json(body)
 }
