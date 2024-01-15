f12125ac43c254f9821188a3d4aa82090f3c134e
antoniomrrds <arqueiroverd@gmail.com>
2024-01-15 00:21:09 -0300

    test: 🧪ensure auth middleware returns 200 if authorization header was provided

--- tests/main/middlewares/authentication.test.ts
+++ tests/main/middlewares/authentication.test.ts
@@ -1,8 +1,10 @@
 import { ForbiddenError } from '@/presentation/errors'
 import { app } from '@/main/config'
+import { auth } from '@/main/middlewares'
+import { jwtSecret } from '@/main/config/env'
 
 import request from 'supertest'
-import { auth } from '@/main/middlewares'
+import { sign } from 'jsonwebtoken'
 
 describe('AuthenticationMiddleware', () => {
   it('should return 403 if authorization header was not provided', async () => {
@@ -11,5 +13,18 @@ describe('AuthenticationMiddleware', () => {
     const { status, body } = await request(app).get('/test-auth')
     expect(status).toBe(403)
     expect(body.error).toEqual(new ForbiddenError().message)
+  })
+  it('Should return 200 if authorization header was provided', async () => {
+    const authorization = sign({ key: 'any_user_id' }, jwtSecret)
+    app.get('/test-auth', auth, (req, res) => {
+      res.json(req.locals)
+    })
+
+    const { status, body } = await request(app)
+      .get('/test-auth')
+      .set('authorization', authorization)
+
+    expect(status).toBe(200)
+    expect(body).toEqual({ userId: 'any_user_id' })
   })
 })
