6cfe5e029bb34725891cf867922fe43244ebee09
antoniomrrds <arqueiroverd@gmail.com>
2024-01-17 16:33:04 -0300

    test: 🧪ensure ChangeProfilePicture calls UploadFile with unique id

--- tests/domain/use-cases/change-profile-picture.spec.ts
+++ tests/domain/use-cases/change-profile-picture.spec.ts
@@ -1,16 +1,19 @@
-import { UploadFile } from '@/domain/ports'
+import { UUIDGenerator, UploadFile } from '@/domain/ports'
 import { setupChangeProfilePicture } from '@/domain/use-cases'
 import { mock } from 'jest-mock-extended'
 
 describe('ChangeProfilePicture', () => {
   it('should call UploadFile with correct input', async () => {
+    const uuid = 'any_unique_id'
     const file = Buffer.from('any_buffer')
     const fileStorage = mock<UploadFile>()
-    const sut = setupChangeProfilePicture(fileStorage)
+    const crypto = mock<UUIDGenerator>()
+    crypto.uuid.mockReturnValue(uuid)
+    const sut = setupChangeProfilePicture(fileStorage, crypto)
 
     await sut({ id: 'any_id', file })
 
-    expect(fileStorage.upload).toHaveBeenCalledWith({ file, key: 'any_id' })
+    expect(fileStorage.upload).toHaveBeenCalledWith({ file, key: uuid })
     expect(fileStorage.upload).toHaveBeenCalledTimes(1)
   })
 })
