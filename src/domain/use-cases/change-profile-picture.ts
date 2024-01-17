17c667d7c090c51474dc5146a4bf45d1840d94df
antoniomrrds <arqueiroverd@gmail.com>
2024-01-17 16:50:02 -0300

    refactor: 📦 isolate SUT creation

--- src/domain/use-cases/change-profile-picture.ts
+++ src/domain/use-cases/change-profile-picture.ts
@@ -2,7 +2,7 @@ import { UUIDGenerator, UploadFile } from '@/domain/ports'
 
 type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator) => ChangeProfilePicture
 type Input = { id: string, file: Buffer }
-type ChangeProfilePicture = (input: Input) => Promise<void>
+export type ChangeProfilePicture = (input: Input) => Promise<void>
 
 export const setupChangeProfilePicture: Setup = (fileStorage, crypto) => async ({ id, file }) => {
   await fileStorage.upload({ file, key: crypto.uuid({ key: id }) })
