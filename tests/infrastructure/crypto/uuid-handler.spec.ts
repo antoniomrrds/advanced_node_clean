c76841eeaa655b31e2a67fc2d4d5887775773c73
antoniomrrds <arqueiroverd@gmail.com>
2024-01-19 00:39:18 -0300

    test: 🧪ensure UUIDHandler returns correct value

--- tests/infrastructure/crypto/uuid-handler.spec.ts
+++ tests/infrastructure/crypto/uuid-handler.spec.ts
@@ -1,4 +1,5 @@
 import { UUIDHandler } from '@/infrastructure/crypto'
+import { mocked } from 'jest-mock'
 import { v4 } from 'uuid'
 
 jest.mock('uuid')
@@ -10,5 +11,13 @@ describe('UUIDHandler', () => {
     sut.uuid({ key: 'any_key' })
 
     expect(v4).toHaveBeenCalledTimes(1)
+  })
+  it('Should return correct uuid', () => {
+    mocked(v4).mockReturnValueOnce('any_uuid')
+    const sut = new UUIDHandler()
+
+    const uuid = sut.uuid({ key: 'any_key' })
+
+    expect(uuid).toBe('any_key_any_uuid')
   })
 })
