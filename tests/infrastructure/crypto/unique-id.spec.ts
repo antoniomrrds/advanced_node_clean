d5aaeabdf434fab96f9c2b65edb09d73b2be3d61
antoniomrrds <arqueiroverd@gmail.com>
2024-01-19 02:17:47 -0300

    test: 🧪ensure UniqueId returns correct uuid

---
+++ tests/infrastructure/crypto/unique-id.spec.ts
@@ -0,0 +1,18 @@
+import { UniqueId } from '@/infrastructure/crypto'
+
+describe('UUIDHandler', () => {
+  it('Should create a unique id', () => {
+    const sut = new UniqueId(new Date(2024, 0, 3, 10, 10, 10))
+
+    const uuid = sut.uuid({ key: 'any_key' })
+
+    expect(uuid).toBe('any_key_20240103101010')
+  })
+  it('Should create a unique id', () => {
+    const sut = new UniqueId(new Date(1996, 9, 23, 18, 1, 0))
+
+    const uuid = sut.uuid({ key: 'any_key' })
+
+    expect(uuid).toBe('any_key_19961023180100')
+  })
+})
