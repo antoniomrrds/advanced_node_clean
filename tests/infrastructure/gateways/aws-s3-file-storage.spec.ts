eee1e2247435f5f8b86d52231007437c6b6600a5
antoniomrrds <arqueiroverd@gmail.com>
2024-01-23 12:21:03 -0300

    test: 🧪 ensure AwsS3FileStorage calls putObject with correct input

--- tests/infrastructure/gateways/aws-s3-file-storage.spec.ts
+++ tests/infrastructure/gateways/aws-s3-file-storage.spec.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/unbound-method */
 import { AwsS3FileStorage } from '@/infrastructure/gateways'
-import { config } from 'aws-sdk'
+import { config, S3 } from 'aws-sdk'
+import { mocked } from 'jest-mock'
 
 jest.mock('aws-sdk')
 
@@ -8,14 +9,25 @@ describe('AwsS3FileStorage', () => {
   let accessKey: string
   let secret: string
   let sut: AwsS3FileStorage
+  let bucket: string
+  let key: string
+  let file: Buffer
+  let putObjectSpy: jest.Mock
+  let putObjectPromiseSpy: jest.Mock
 
   beforeAll(() => {
     accessKey = 'any_access_key'
     secret = 'any_secret'
+    bucket = 'any_bucket'
+    key = 'any_key'
+    file = Buffer.from('any_buffer')
+    putObjectPromiseSpy = jest.fn()
+    putObjectSpy = jest.fn().mockImplementation(() => ({ promise: putObjectPromiseSpy }))
+    mocked(S3).mockImplementation(jest.fn().mockImplementation(() => ({ putObject: putObjectSpy })))
   })
 
   beforeEach(() => {
-    sut = new AwsS3FileStorage(accessKey, secret)
+    sut = new AwsS3FileStorage(accessKey, secret, bucket)
   })
 
   it('Should config aws credentials on creation', () => {
@@ -28,5 +40,17 @@ describe('AwsS3FileStorage', () => {
       }
     })
     expect(config.update).toHaveBeenCalledTimes(1)
+  })
+  it('Should call putObject with correct input', async () => {
+    await sut.upload({ key, file })
+
+    expect(putObjectSpy).toHaveBeenCalledWith({
+      Bucket: bucket,
+      Key: key,
+      Body: file,
+      ACL: 'public-read'
+    })
+    expect(putObjectSpy).toHaveBeenCalledTimes(1)
+    expect(putObjectPromiseSpy).toHaveBeenCalledTimes(1)
   })
 })
