3f43e3639995775188096c65af6cd1b11e728e72
antoniomrrds <arqueiroverd@gmail.com>
2024-01-26 15:50:45 -0300

    refactor: 📦separate pgDataSourceOptions from the rest of the common configurations

--- src/infrastructure/repositories/postgres/connection/data-source.ts
+++ src/infrastructure/repositories/postgres/connection/data-source.ts
@@ -1,15 +1,18 @@
 import { DB } from '@/main/config/env'
 import { DataSourceOptions, DataSource } from 'typeorm'
 
-const dataSourceOptions: DataSourceOptions = {
+const pgDataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   host: DB.host,
   port: DB.port,
   username: DB.username,
   password: DB.password,
-  database: DB.database,
+  database: DB.database
+}
+const dataSourceOptions: DataSourceOptions = {
+  ...pgDataSourceOptions,
   entities: [
-    'dist/infrastructure/repositories/postgres/entities/**/*{.ts,.js}'
+      `${process.env.TS_NODE_DEV === undefined ? 'src' : 'dist'}/infrastructure/repositories/postgres/entities/**/*{.ts,.js}`
   ],
   synchronize: true
 }
