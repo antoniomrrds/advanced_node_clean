95cc9807356380e9bcaa5900b95ebeb6671d7f59
antoniomrrds <arqueiroverd@gmail.com>
2024-01-27 12:47:56 -0300

    feat: ​​🧩 ensure PgUserProfileRepository loads user profile

--- src/infrastructure/repositories/postgres/repositories/user-profile.ts
+++ src/infrastructure/repositories/postgres/repositories/user-profile.ts
@@ -1,11 +1,17 @@
-import { SaveUserPicture } from '@/domain/ports'
+import { LoadUserProfile, SaveUserPicture } from '@/domain/ports'
 import { PgUser, PostgresDataSource } from '@/infrastructure/repositories/postgres'
 
-export class PgUserProfileRepository implements SaveUserPicture {
+export class PgUserProfileRepository implements SaveUserPicture, LoadUserProfile {
   async savePicture ({ id, initials, pictureUrl }: SaveUserPicture.Input): Promise<void> {
     /* istanbul ignore next */
     const value = { initials: initials ?? null!, pictureUrl: pictureUrl ?? null! }
     const pgUserRepo = PostgresDataSource.getRepository(PgUser)
     await pgUserRepo.update({ id: Number(id) }, { ...value })
+  }
+
+  async load ({ id }: LoadUserProfile.Input): Promise< LoadUserProfile.Output> {
+    const pgUserRepo = PostgresDataSource.getRepository(PgUser)
+    const pgUser = await pgUserRepo.findOne({ where: { id: Number(id) } })
+    if (pgUser !== null) return { name: pgUser.name }
   }
 }
