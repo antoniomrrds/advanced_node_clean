c0281957ec61e7bcd481d76e07558c016a0d9bfa
antoniomrrds <arqueiroverd@gmail.com>
2024-01-27 15:06:13 -0300

    fix: 🐛 change savePicture update method to raw query

--- src/infrastructure/repositories/postgres/repositories/user-profile.ts
+++ src/infrastructure/repositories/postgres/repositories/user-profile.ts
@@ -1,12 +1,9 @@
 import { LoadUserProfile, SaveUserPicture } from '@/domain/ports'
 import { PgUser, PostgresDataSource } from '@/infrastructure/repositories/postgres'
-
 export class PgUserProfileRepository implements SaveUserPicture, LoadUserProfile {
   async savePicture ({ id, initials, pictureUrl }: SaveUserPicture.Input): Promise<void> {
-    /* istanbul ignore next */
-    const value = { initials: initials ?? null!, pictureUrl: pictureUrl ?? null! }
     const pgUserRepo = PostgresDataSource.getRepository(PgUser)
-    await pgUserRepo.update({ id: Number(id) }, { ...value })
+    await pgUserRepo.query('UPDATE usuarios SET iniciais = $1, foto = $2 WHERE id = $3 ', [initials, pictureUrl, Number(id)])
   }
 
   async load ({ id }: LoadUserProfile.Input): Promise< LoadUserProfile.Output> {
