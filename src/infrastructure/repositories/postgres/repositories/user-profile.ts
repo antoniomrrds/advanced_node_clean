dcee3cd8213bf0c7319b57bad80498c2b2edace9
antoniomrrds <arqueiroverd@gmail.com>
2024-01-27 05:32:01 -0300

    feat: ​​🧩ensure PgUserProfileRepository updates user profile

---
+++ src/infrastructure/repositories/postgres/repositories/user-profile.ts
@@ -0,0 +1,10 @@
+import { SaveUserPicture } from '@/domain/ports'
+import { PgUser, PostgresDataSource } from '@/infrastructure/repositories/postgres'
+
+export class PgUserProfileRepository implements SaveUserPicture {
+  async savePicture ({ id, initials, pictureUrl }: SaveUserPicture.Input): Promise<void> {
+    const value = { initials: initials ?? null!, pictureUrl: pictureUrl ?? null! }
+    const pgUserRepo = PostgresDataSource.getRepository(PgUser)
+    await pgUserRepo.update({ id: Number(id) }, { ...value })
+  }
+}
