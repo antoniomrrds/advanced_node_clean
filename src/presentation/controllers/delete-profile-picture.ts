49a1ac25102b7a61b1ae62552746478c48ab847d
antoniomrrds <arqueiroverd@gmail.com>
2024-01-29 00:41:46 -0300

    feat: ​​🧩ensure DeletePictureController returns 204

--- src/presentation/controllers/delete-profile-picture.ts
+++ src/presentation/controllers/delete-profile-picture.ts
@@ -1,11 +1,14 @@
 import { ChangeProfilePicture } from '@/domain/use-cases'
+import { noContent } from '@/presentation/helpers'
+import { HttpResponse } from '@/presentation/ports'
 
 type HttpRequest = { userId: string }
 
 export class DeletePictureController {
   constructor (private readonly changeProfilePicture: ChangeProfilePicture) {}
 
-  async handle ({ userId }: HttpRequest): Promise<void> {
+  async handle ({ userId }: HttpRequest): Promise<HttpResponse> {
     await this.changeProfilePicture({ id: userId })
+    return noContent()
   }
 }
