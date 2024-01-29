1485737b7b43143f81cbdc0a7c00c94a0e7fb04c
antoniomrrds <arqueiroverd@gmail.com>
2024-01-29 11:05:09 -0300

    test: 🧪ensure SavePictureController returns 400 if type is invalid

--- tests/presentation/controllers/save-picture.spec.ts
+++ tests/presentation/controllers/save-picture.spec.ts
@@ -1,21 +1,31 @@
 import { SavePictureController } from '@/presentation/controllers'
-import { RequiredFieldError } from '@/presentation/errors'
+import { InvalidMimeTypeError, RequiredFieldError } from '@/presentation/errors'
 
 describe('SavePictureController', () => {
+  let buffer: Buffer
+  const mimeType = 'image/png'
   let sut: SavePictureController
   beforeEach(() => {
     sut = new SavePictureController()
   })
+  beforeAll(() => {
+    buffer = Buffer.from('any_file')
+  })
 
   describe.each([
-    ['file is undefined', undefined] as const,
-    ['file is null', null] as const,
-    ['file is empty', { buffer: Buffer.from('') }] as const
+    ['file is undefined', undefined],
+    ['file is null', null],
+    ['file is empty', { buffer: Buffer.from(''), mimeType }]
   ])('File is invalid', (testName, file: any) => {
     it(`Should return 400 if ${testName}`, async () => {
       const response = await sut.handle({ file })
 
       expect(response).toEqual({ statusCode: 400, body: new RequiredFieldError('file') })
     })
+  })
+  it('Should return 400 if type is invalid', async () => {
+    const response = await sut.handle({ file: { buffer, mimeType: 'invalid_type' } })
+
+    expect(response).toEqual({ statusCode: 400, body: new InvalidMimeTypeError(['png, jpeg']) })
   })
 })
