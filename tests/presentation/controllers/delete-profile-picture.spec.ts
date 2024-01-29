0ef15965e888526812f9865c505da1123c4dcaa8
antoniomrrds <arqueiroverd@gmail.com>
2024-01-29 00:42:46 -0300

    test: 🧪ensure DeletePictureController returns 204

--- tests/presentation/controllers/delete-profile-picture.spec.ts
+++ tests/presentation/controllers/delete-profile-picture.spec.ts
@@ -17,4 +17,9 @@ describe('DeletePictureController', () => {
     expect(changeProfilePicture).toHaveBeenCalledWith({ id: 'any_user_id' })
     expect(changeProfilePicture).toHaveBeenCalledTimes(1)
   })
+  it('Should return 204 on success', async () => {
+    const response = await sut.handle({ userId: 'any_user_id' })
+
+    expect(response).toEqual({ statusCode: 204, body: null })
+  })
 })
