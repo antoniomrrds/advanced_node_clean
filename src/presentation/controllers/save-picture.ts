70aa01f0cda1c945fe896efafbb08dcb5ac51c75
antoniomrrds <arqueiroverd@gmail.com>
2024-01-29 10:51:31 -0300

    feat: 🧩 ensure SavePictureController returns 400 if file mime type is invalid

--- src/presentation/controllers/save-picture.ts
+++ src/presentation/controllers/save-picture.ts
@@ -1,12 +1,14 @@
-import { RequiredFieldError } from '@/presentation/errors'
+import { InvalidMimeTypeError, RequiredFieldError } from '@/presentation/errors'
 import { badRequest } from '@/presentation/helpers'
 import { HttpResponse } from '@/presentation/ports'
 
-type HttpRequest = { file: { buffer: Buffer } }
+type HttpRequest = { file: { buffer: Buffer, mimeType: string } }
 type Model = Error
 
 export class SavePictureController {
   async handle ({ file }: HttpRequest): Promise<HttpResponse<Model>> {
-    return badRequest(new RequiredFieldError('file'))
+    if (file === undefined || file === null) return badRequest(new RequiredFieldError('file'))
+    if (file.buffer.length === 0) return badRequest(new RequiredFieldError('file'))
+    return badRequest(new InvalidMimeTypeError(['png', 'jpeg']))
   }
 }
