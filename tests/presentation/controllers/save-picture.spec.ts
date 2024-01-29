103063d75bfc28c0e4538ae3b31cbcc0c82c15a3
antoniomrrds <arqueiroverd@gmail.com>
2024-01-29 01:21:40 -0300

    refactor: 📦 isolate SUT creation

--- tests/presentation/controllers/save-picture.spec.ts
+++ tests/presentation/controllers/save-picture.spec.ts
@@ -2,9 +2,11 @@ import { SavePictureController } from '@/presentation/controllers'
 import { RequiredFieldError } from '@/presentation/errors'
 
 describe('SavePictureController', () => {
+  let sut: SavePictureController
+  beforeEach(() => {
+    sut = new SavePictureController()
+  })
   it('Should return 400 if file is not provided', async () => {
-    const sut = new SavePictureController()
-
     const response = await sut.handle({ file: undefined })
 
     expect(response).toEqual({ statusCode: 400, body: new RequiredFieldError('file') })
