d3cd2e599d846ebb1854ecdb0f0d0964be045128
antoniomrrds <arqueiroverd@gmail.com>
2024-01-31 20:03:56 -0300

    test: 🧪 ensure AllowedMimeTypes returns undefined if  values are allowed

--- tests/presentation/validation/allowed-mime-types.spec.ts
+++ tests/presentation/validation/allowed-mime-types.spec.ts
@@ -1,5 +1,5 @@
 import { InvalidMimeTypeError } from '@/presentation/errors'
-import { AllowedMimeTypes } from '@/presentation/validation'
+import { AllowedMimeTypes, Extension } from '@/presentation/validation'
 
 describe('AllowedMimeTypes', () => {
   it('Should return an InvalidMimeTypeError if value is not allowed', () => {
@@ -8,5 +8,21 @@ describe('AllowedMimeTypes', () => {
     const error = sut.validate()
 
     expect(error).toEqual(new InvalidMimeTypeError(['png']))
+  })
+
+  type TestData = { name: string, type: Extension, expected: string }
+
+  const testCases: TestData[] = [
+    { name: 'png', type: 'png', expected: 'image/png' },
+    { name: 'jpeg', type: 'jpg', expected: 'image/jpeg' },
+    { name: 'jpg', type: 'jpg', expected: 'image/jpg' }
+  ]
+
+  it.each(testCases)('Should return undefined if the value equals $name because it is allowed', ({ type, expected }) => {
+    const sut = new AllowedMimeTypes([type], expected)
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
   })
 })
