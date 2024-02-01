302f00b814cad95aad9bd5cc2a2b107be64cb54b
antoniomrrds <arqueiroverd@gmail.com>
2024-02-01 17:09:17 -0300

    refactor: 📦 create variables to avoid duplication of test code

--- tests/presentation/validation/builder.spec.ts
+++ tests/presentation/validation/builder.spec.ts
@@ -1,6 +1,19 @@
-import { AllowedMimeTypes, MaxFileSize, Required, RequiredBuffer, RequiredString, ValidationBuilder } from '@/presentation/validation'
+import {
+  AllowedMimeTypes, Extension, MaxFileSize, Required, RequiredBuffer, RequiredString, ValidationBuilder
+} from '@/presentation/validation'
 
 describe('ValidationBuilder', () => {
+  let buffer: Buffer
+  let mimeType: string
+  let maxSizeInMb: number
+  let allowed: Extension[]
+
+  beforeAll(() => {
+    buffer = Buffer.from('any_value')
+    mimeType = 'image/png'
+    maxSizeInMb = 6
+    allowed = ['png']
+  })
   it('should return RequiredString', () => {
     const validators = ValidationBuilder
       .of({ value: 'any_value' })
@@ -10,17 +23,6 @@ describe('ValidationBuilder', () => {
     expect(validators).toEqual([new RequiredString('any_value')])
   })
   it('should return RequiredBuffer', () => {
-    const buffer = Buffer.from('any_value')
-    const validators = ValidationBuilder
-      .of({ value: buffer })
-      .required()
-      .build()
-
-    expect(validators).toEqual([new RequiredBuffer(buffer)])
-  })
-  it('should return RequiredBuffer', () => {
-    const buffer = Buffer.from('any_value')
-
     const validators = ValidationBuilder
       .of({ value: buffer })
       .required()
@@ -36,9 +38,7 @@ describe('ValidationBuilder', () => {
 
     expect(validators).toEqual([new Required({ any: 'value' })])
   })
-  it('Should return Required', () => {
-    const buffer = Buffer.from('any_value')
-
+  it('Should return Required and RequiredBuffer when exists buffer', () => {
     const validators = ValidationBuilder
       .of({ value: { buffer } })
       .required()
@@ -49,34 +49,30 @@ describe('ValidationBuilder', () => {
       new RequiredBuffer(buffer)])
   })
   it('Should return correct image validators', () => {
-    const buffer = Buffer.from('any_value')
-
     const validators = ValidationBuilder
       .of({ value: { buffer } })
-      .image({ allowed: ['png'], maxSizeInMb: 6 })
+      .image({ allowed, maxSizeInMb })
       .build()
 
-    expect(validators).toEqual([new MaxFileSize(6, buffer)])
+    expect(validators).toEqual([new MaxFileSize(maxSizeInMb, buffer)])
   })
   it('Should return correct image validators', () => {
     const validators = ValidationBuilder
-      .of({ value: { mimeType: 'image/png' } })
-      .image({ allowed: ['png'], maxSizeInMb: 6 })
+      .of({ value: { mimeType } })
+      .image({ allowed, maxSizeInMb })
       .build()
 
-    expect(validators).toEqual([new AllowedMimeTypes(['png'], 'image/png')])
+    expect(validators).toEqual([new AllowedMimeTypes(allowed, mimeType)])
   })
   it('Should return correct image validators', () => {
-    const buffer = Buffer.from('any_value')
-
     const validators = ValidationBuilder
-      .of({ value: { buffer, mimeType: 'image/png' } })
-      .image({ allowed: ['png'], maxSizeInMb: 6 })
+      .of({ value: { buffer, mimeType } })
+      .image({ allowed, maxSizeInMb })
       .build()
 
     expect(validators).toEqual([
-      new AllowedMimeTypes(['png'], 'image/png'),
-      new MaxFileSize(6, buffer)
+      new AllowedMimeTypes(allowed, mimeType),
+      new MaxFileSize(maxSizeInMb, buffer)
     ])
   })
 })
