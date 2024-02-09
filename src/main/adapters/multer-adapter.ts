c4a28d4f4f974425de1ab23e7df307189f4609c1
antoniomrrds <arqueiroverd@gmail.com>
2024-02-09 14:06:30 -0300

    refactor: 📦improve code readability

--- src/main/adapters/multer-adapter.ts
+++ src/main/adapters/multer-adapter.ts
@@ -1,16 +1,21 @@
 import { ServerError } from '@/presentation/errors'
-import { RequestHandler } from 'express'
+import { NextFunction, Request, RequestHandler, Response } from 'express'
 import multer from 'multer'
 
-export const adaptMulter: RequestHandler = (req, res, next) => {
+const handleUploadError = (res: Response, error: Error): Response => {
+  return res.status(500).json({ error: new ServerError(error).message })
+}
+
+const attachFileToLocals = (req: Request): void => {
+  req.file && (req.locals = { ...req.locals, file: { buffer: req.file.buffer, mimeType: req.file.mimetype } })
+}
+
+export const adaptMulter: RequestHandler = (req, res, next: NextFunction) => {
   const upload = multer().single('picture')
+
   upload(req, res, error => {
-    if (error !== undefined) {
-      return res.status(500).json({ error: new ServerError(error as Error).message })
-    }
-    if (req.file !== undefined) {
-      req.locals = { ...req.locals, file: { buffer: req.file.buffer, mimeType: req.file.mimetype } }
-    }
+    if (error instanceof Error) handleUploadError(res, error)
+    attachFileToLocals(req)
     next()
   })
 }
