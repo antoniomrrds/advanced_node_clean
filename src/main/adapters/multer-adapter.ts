8186e581dcfb374cbe2a20218f192af0adf8e828
antoniomrrds <arqueiroverd@gmail.com>
2024-02-09 11:39:58 -0300

    feat: ​​🧩  ensure MulterAdapter returns 500 if upload fails

--- src/main/adapters/multer-adapter.ts
+++ src/main/adapters/multer-adapter.ts
@@ -1,7 +1,11 @@
+import { ServerError } from '@/presentation/errors'
 import { RequestHandler } from 'express'
 import multer from 'multer'
 
 export const adaptMulter: RequestHandler = (req, res, next) => {
   const upload = multer().single('picture')
-  upload(req, res, () => {})
+  upload(req, res, error => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    res.status(500).json({ error: new ServerError(error).message })
+  })
 }
