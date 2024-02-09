5d3d0b4264c839fc794466c5c909083440bd2ac0
antoniomrrds <arqueiroverd@gmail.com>
2024-02-09 16:41:28 -0300

    test: 🧪ensure PUT /api/users/picture returns 200 on success

--- tests/main/routes/user.test.ts
+++ tests/main/routes/user.test.ts
@@ -1,15 +1,15 @@
-import request from 'supertest'
 import { Repository } from 'typeorm'
+import { Express } from 'express'
 import { sign } from 'jsonwebtoken'
+import request from 'supertest'
 
-import { PgTestHelper } from '@/tests/infrastructure/repositories/postgres'
 import { PgUser, PostgresDataSource } from '@/infrastructure/repositories/postgres'
+import { PgTestHelper } from '@/tests/infrastructure/repositories/postgres'
 import { jwtSecret } from '@/main/config/env'
-import { app } from '@/main/config'
 
 describe('User Routes', () => {
+  let app: Express
   let pgUserRepo: Repository<PgUser>
-
   beforeAll(async () => {
     await PgTestHelper.connect([PgUser])
     pgUserRepo = PgTestHelper.connection.getRepository(PgUser)
@@ -17,6 +17,7 @@ describe('User Routes', () => {
   })
 
   beforeEach(async () => {
+    app = (await import('@/main/config')).app
     PgTestHelper.restore()
   })
 
@@ -43,11 +44,30 @@ describe('User Routes', () => {
     })
   })
   describe('PUT /api/users/picture', () => {
+    const uploadSpy = jest.fn()
+
+    jest.mock('@/infrastructure/gateways/aws-s3-file-storage', () => ({
+      AwsS3FileStorage: jest.fn().mockReturnValue({ upload: uploadSpy })
+    }))
+
     it('Should return 403 if no authorization header is present', async () => {
       const { status } = await request(app)
         .put('/api/users/picture')
 
       expect(status).toBe(403)
+    })
+    it('Should return 200 with valid data ', async () => {
+      uploadSpy.mockResolvedValueOnce('any_url')
+      const { id } = await pgUserRepo.save({ email: 'any_email', name: 'any name' })
+
+      const authorization = sign({ key: id }, jwtSecret)
+      const { status, body } = await request(app)
+        .put('/api/users/picture')
+        .set({ authorization })
+        .attach('picture', Buffer.from('any_buffer'), { filename: 'any_name', contentType: 'image/jpg' })
+
+      expect(status).toBe(200)
+      expect(body).toEqual({ pictureUrl: 'any_url', initials: undefined })
     })
   })
 })
