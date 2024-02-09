0430dca63f63d9d123b1d7d414f0641b200e76a6
antoniomrrds <arqueiroverd@gmail.com>
2024-02-09 12:42:47 -0300

    feat: ​​🧩  ensure MulterAdapter add file to req.locals

--- src/main/adapters/multer-adapter.ts
+++ src/main/adapters/multer-adapter.ts
@@ -5,7 +5,11 @@ import multer from 'multer'
 export const adaptMulter: RequestHandler = (req, res, next) => {
   const upload = multer().single('picture')
   upload(req, res, error => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    res.status(500).json({ error: new ServerError(error).message })
+    if (error !== undefined) {
+      return res.status(500).json({ error: new ServerError(error as Error).message })
+    }
+    if (req.file !== undefined) {
+      req.locals = { ...req.locals, file: { buffer: req.file.buffer, mimeType: req.file.mimetype } }
+    }
   })
 }
