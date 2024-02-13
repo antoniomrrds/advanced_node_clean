4383b8836c6a9e760c4b5c95fa6e1e226b8ff700
antoniomrrds <arqueiroverd@gmail.com>
2024-02-13 17:27:35 -0300

    test: 🧪ensure PgConnection use existing connection

--- tests/infrastructure/repositories/postgres/helpers/connection.spec.ts
+++ tests/infrastructure/repositories/postgres/helpers/connection.spec.ts
@@ -1,19 +1,53 @@
-/* eslint-disable @typescript-eslint/unbound-method */
 import { PgConnection } from '@/infrastructure/repositories/postgres/helpers'
-import { PostgresDataSource } from '@/infrastructure/repositories/postgres/connection'
+import { mocked } from 'jest-mock'
+import { DataSource, DataSourceOptions } from 'typeorm'
 
-jest.mock('@/infrastructure/repositories/postgres/connection')
+jest.mock('@/infrastructure/repositories/postgres/connection', () => ({
+  dataSourceOptions: {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'user',
+    password: 'password',
+    database: 'database',
+    entities: ['any-path']
+  }
+}))
+
+jest.mock('typeorm', () => ({
+  DataSource: jest.fn(),
+  Entity: jest.fn(),
+  PrimaryGeneratedColumn: jest.fn(),
+  Column: jest.fn()
+}))
+
+class MockDataSource extends DataSource {
+  isInitialized = false
+
+  initialize = jest.fn()
+  createQueryRunner = jest.fn()
+}
 
 describe('PgConnection', () => {
-  let PostgresDataSourceSpy: jest.Mocked<typeof PostgresDataSource>
   let sut: PgConnection
-  let initializeSpy: jest.SpyInstance
-  let createQueryRunnerSpy: jest.SpyInstance
+  let dataSourceSpy: jest.Mock
+  let mockDataSourceSpy: MockDataSource
+  const dataSourceOptionsMock: DataSourceOptions = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'user',
+    password: 'password',
+    database: 'database',
+    entities: ['any-path']
+  }
 
   beforeAll(() => {
-    PostgresDataSourceSpy = PostgresDataSource as jest.Mocked<typeof PostgresDataSource>
-    initializeSpy = PostgresDataSourceSpy.initialize
-    createQueryRunnerSpy = PostgresDataSourceSpy.createQueryRunner
+    const mockDataSource = new MockDataSource(dataSourceOptionsMock)
+    dataSourceSpy = jest.fn().mockReturnValue(mockDataSource)
+    mocked(DataSource).mockImplementation(dataSourceSpy)
+
+    mockDataSourceSpy = mockDataSource
   })
 
   beforeEach(() => {
@@ -22,16 +56,26 @@ describe('PgConnection', () => {
 
   it('Should have only one instance', () => {
     const connection2 = PgConnection.getInstance()
-
     expect(sut).toBe(connection2)
   })
 
   it('Should create a new connection', async () => {
     await sut.connect()
 
-    expect(initializeSpy).toHaveBeenCalledWith()
-    expect(initializeSpy).toHaveBeenCalledTimes(1)
-    expect(createQueryRunnerSpy).toHaveBeenCalledWith()
-    expect(createQueryRunnerSpy).toHaveBeenCalledTimes(1)
+    expect(mockDataSourceSpy.initialize).toHaveBeenCalledTimes(1)
+    expect(dataSourceSpy).toHaveBeenCalledWith(dataSourceOptionsMock)
+    expect(dataSourceSpy).toHaveBeenCalledTimes(1)
+    expect(mockDataSourceSpy.createQueryRunner).toHaveBeenCalledWith()
+    expect(mockDataSourceSpy.createQueryRunner).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should use an existing connection', async () => {
+    mockDataSourceSpy.isInitialized = true
+
+    await sut.connect()
+
+    expect(mockDataSourceSpy.initialize).not.toHaveBeenCalled()
+    expect(mockDataSourceSpy.createQueryRunner).toHaveBeenCalledWith()
+    expect(mockDataSourceSpy.createQueryRunner).toHaveBeenCalledTimes(1)
   })
 })
