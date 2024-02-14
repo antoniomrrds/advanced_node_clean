e8049c0ea9a917c481ce0b689d912c30497480dd
antoniomrrds <arqueiroverd@gmail.com>
2024-02-14 13:32:45 -0300

    test: 🧪 ensure PgConnection returns ConnectionNotFoundError on disconnect

--- tests/infrastructure/repositories/postgres/helpers/connection.spec.ts
+++ tests/infrastructure/repositories/postgres/helpers/connection.spec.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/dot-notation */
-import { PgConnection } from '@/infrastructure/repositories/postgres/helpers'
+import { ConnectionNotFoundError, PgConnection } from '@/infrastructure/repositories/postgres'
 import { mocked } from 'jest-mock'
 import { DataSource, DataSourceOptions } from 'typeorm'
 
-jest.mock('@/infrastructure/repositories/postgres/connection', () => ({
-  dataSourceOptions: {
+function getDataSourceOptions (): DataSourceOptions {
+  return {
     type: 'postgres',
     host: 'localhost',
     port: 5432,
@@ -13,6 +13,10 @@ jest.mock('@/infrastructure/repositories/postgres/connection', () => ({
     database: 'database',
     entities: ['any-path']
   }
+}
+
+jest.mock('@/infrastructure/repositories/postgres/connection', () => ({
+  dataSourceOptions: getDataSourceOptions()
 }))
 
 jest.mock('typeorm', () => ({
@@ -34,17 +38,10 @@ describe('PgConnection', () => {
   let sut: PgConnection
   let dataSourceSpy: jest.Mock
   let mockDataSourceSpy: MockDataSource
-  const dataSourceOptionsMock: DataSourceOptions = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'user',
-    password: 'password',
-    database: 'database',
-    entities: ['any-path']
-  }
+  let dataSourceOptionsMock: DataSourceOptions
 
   beforeAll(() => {
+    dataSourceOptionsMock = getDataSourceOptions()
     const mockDataSource = new MockDataSource(dataSourceOptionsMock)
     dataSourceSpy = jest.fn().mockReturnValue(mockDataSource)
     mocked(DataSource).mockImplementation(dataSourceSpy)
@@ -60,11 +57,9 @@ describe('PgConnection', () => {
     const connection2 = PgConnection.getInstance()
     expect(sut).toBe(connection2)
   })
-
   it('Should not have a connection before connect is called', () => {
     expect(sut['connection']).toBeUndefined()
   })
-
   it('Should create a new connection', async () => {
     await sut.connect()
 
@@ -74,7 +69,6 @@ describe('PgConnection', () => {
     expect(mockDataSourceSpy.createQueryRunner).toHaveBeenCalledWith()
     expect(mockDataSourceSpy.createQueryRunner).toHaveBeenCalled()
   })
-
   it('Should use an existing connection', async () => {
     mockDataSourceSpy.isInitialized = true
 
@@ -84,14 +78,21 @@ describe('PgConnection', () => {
     expect(mockDataSourceSpy.createQueryRunner).toHaveBeenCalledWith()
     expect(mockDataSourceSpy.createQueryRunner).toHaveBeenCalled()
   })
-
   it('Should close the connection', async () => {
     await sut.connect()
-    await sut.close()
+    await sut.disconnect()
 
     expect(mockDataSourceSpy.destroy).toHaveBeenCalledWith()
     expect(mockDataSourceSpy.destroy).toHaveBeenCalled()
     expect(sut['query']).toBeUndefined()
     expect(sut['connection']).toBeUndefined()
+  })
+  it('Should return ConnectionNotFoundError on disconnect if connection is not found', async () => {
+    const promise = sut.disconnect()
+
+    expect(sut['query']).toBeUndefined()
+    expect(sut['connection']).toBeUndefined()
+    expect(mockDataSourceSpy.destroy).not.toHaveBeenCalled()
+    await expect(promise).rejects.toThrow(new ConnectionNotFoundError())
   })
 })
