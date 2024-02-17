7ba0cd904f60a70f1b3c49f7d40b1c99854821e1
antoniomrrds <arqueiroverd@gmail.com>
2024-02-17 16:45:58 -0300

    feat: ​​🧩add abstract class PgRepository

---
+++ src/infrastructure/repositories/postgres/repository.ts
@@ -0,0 +1,10 @@
+import { PgConnection } from '@/infrastructure/repositories/postgres/helpers'
+import { EntityTarget, ObjectLiteral, Repository } from 'typeorm'
+
+export abstract class PgRepository {
+  constructor (private readonly connection: PgConnection = PgConnection.getInstance()) {}
+
+  getRepository<Entity extends ObjectLiteral>(entity: EntityTarget<Entity>): Repository<Entity> {
+    return this.connection.getRepository(entity)
+  }
+}
