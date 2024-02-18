e81ee924e35fb7229fef29cc68ddec45d63b3d95
antoniomrrds <arqueiroverd@gmail.com>
2024-02-18 17:11:13 -0300

    feat: ​​🧩 ensure DbTransactionControllerDecorator executes decoratee

--- src/presentation/decorators/db-transaction-controller-decorator.ts
+++ src/presentation/decorators/db-transaction-controller-decorator.ts
@@ -1,8 +1,15 @@
+import { Controller } from '@/presentation/controllers'
 import { DBTransaction } from '@/presentation/ports'
 
 export class DbTransactionControllerDecorator {
-  constructor (private readonly db: DBTransaction) {}
+  constructor (
+    private readonly decoratee: Controller,
+    private readonly db: DBTransaction
+
+  ) {}
+
   async perform (httpRequest: any): Promise<void> {
     await this.db.openTransaction()
+    await this.decoratee.perform(httpRequest)
   }
 }
